dbb8d62ba9283829fc5e2eaa93d486dcaa419978
siphomateke <siphomateke@gmail.com>
2019-10-05 17:14:56 +0200

    Add support for markup regions to ProcessWire module

--- TemplateEngineProcesswire.module.php
+++ TemplateEngineProcesswire.module.php
@@ -23,6 +23,7 @@ class TemplateEngineProcesswire extends TemplateEngine implements Module, Config
     public function initEngine()
     {
         $this->template = new TemplateFile($this->getTemplatesPath() . $this->getFilename());
+        $this->addHookAfter('TemplateFile::render', $this, 'hookRender');
     }
 
 
@@ -32,6 +33,68 @@ class TemplateEngineProcesswire extends TemplateEngine implements Module, Config
     public function set($key, $value)
     {
         $this->template->set($key, $value);
+    }
+
+
+    /**
+	 * Populate markup regions
+	 * 
+	 * @param $html
+	 * 
+	 */
+    protected function populateMarkupRegions($html) {
+		$markupRegions = new WireMarkupRegions();
+		$this->wire($markupRegions);
+		
+		$pos = stripos($html, '<!DOCTYPE html');
+		
+		if($pos === false) {
+			// if no doctype match, attempt an html tag match
+			$pos = stripos($html, '<html'); 
+		}
+		
+		// if no document start, or document starts at pos==0, then nothing to populate
+		if(!$pos) {
+			// there still may be region related stuff that needs to be removed like <region> tags
+			$markupRegions->removeRegionTags($html);
+			return;
+		}
+		
+		// split document at doctype/html boundary
+		$htmlBefore = substr($html, 0, $pos);
+		$html = substr($html, $pos);
+		$options = array('useClassActions' => true); 
+		$config = $this->wire('config');
+		$version = (int) $config->useMarkupRegions;
+		
+		if($config->installed >= 1498132609 || $version >= 2) {
+			// If PW installed after June 21, 2017 do not use legacy class actions
+			// as they are no longer part of the current WireMarkupRegions spec.
+			// Can also force this behavior by setting $config->useMarkupRegions = 2;
+			$options['useClassActions'] = false;
+		}
+		
+        $markupRegions->populate($html, $htmlBefore, $options);
+
+        return $html;
+	}
+    
+
+    /**
+     * Method executed after TemplateFile::render()
+     *
+     * @param HookEvent $event
+     */
+    public function hookRender(HookEvent $event)
+    {
+        $data = $event->return;
+
+        if($this->wire('config')->useMarkupRegions) {
+			$contentType = $template->contentType; 
+			if(empty($contentType) || stripos($contentType, 'html') !== false) {
+				$this->populateMarkupRegions($data);
+			}
+		}
     }
 
 
